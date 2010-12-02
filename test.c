521247d7cddec6130e4aa38557fd3243c64904e4
Bradford Larsen <brad.larsen@gmail.com>
2010-12-02 18:09:11 -0500

    Bugfix in output.

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-static const unsigned NUM_VARS = 10;
+static const unsigned NUM_VARS = 100;
 
 int
 main (int argc, char *argv[])
@@ -17,8 +17,9 @@ main (int argc, char *argv[])
     const unsigned old_num_nodes = bdd_manager_get_num_nodes(mgr);
     printf ("%u nodes\n", old_num_nodes);
     for (unsigned i = 0; i < NUM_VARS; i += 1) {
-        bdd ivar = bdd_manager_get_ith_var (mgr, NUM_VARS - i - 1);
-        printf ("the BDD for variable %u has index %u\n", i, ivar);
+        const unsigned idx = NUM_VARS - i - 1;
+        bdd ivar = bdd_manager_get_ith_var (mgr, idx);
+        printf ("the BDD for variable %u has index %u\n", idx, ivar);
     }
     const unsigned new_num_nodes = bdd_manager_get_num_nodes(mgr);
     printf ("%u nodes\n", new_num_nodes);
