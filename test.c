642ba8e9181252bb86f328b2cd3dd97a084f7de8
Bradford Larsen <brad.larsen@gmail.com>
2010-12-02 14:48:39 -0500

    More test code.

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include "bdd.h"
 #include <stdio.h>
+#include <assert.h>
 
 static const unsigned NUM_VARS = 10;
 
@@ -13,7 +14,15 @@ main (int argc, char *argv[])
         bdd ivar = bdd_manager_get_ith_var (mgr, i);
         printf ("the BDD for variable %u has index %u\n", i, ivar);
     }
-    printf ("%u nodes\n", bdd_manager_get_num_nodes(mgr));
+    const unsigned old_num_nodes = bdd_manager_get_num_nodes(mgr);
+    printf ("%u nodes\n", old_num_nodes);
+    for (unsigned i = 0; i < NUM_VARS; i += 1) {
+        bdd ivar = bdd_manager_get_ith_var (mgr, NUM_VARS - i - 1);
+        printf ("the BDD for variable %u has index %u\n", i, ivar);
+    }
+    const unsigned new_num_nodes = bdd_manager_get_num_nodes(mgr);
+    printf ("%u nodes\n", new_num_nodes);
+    assert (old_num_nodes == new_num_nodes);
     bdd_manager_destroy (mgr);
 
     return 0;
