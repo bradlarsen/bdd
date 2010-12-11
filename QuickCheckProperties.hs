53d91c80362663a905f36c6b09b2f2c55f28928a
Bradford Larsen <brad.larsen@gmail.com>
2010-12-10 23:11:23 -0500

    Eliminated qualified import of BDD.Raw.
    Easier readability, and perhaps it isn't such an issue that I'm
    importing the module unqualified, as it comes from the same package.

--- QuickCheckProperties.hs
+++ QuickCheckProperties.hs
@@ -1,7 +1,7 @@
 module QuickCheckProperties where
 
 import BoolExpr
-import qualified BDD.Raw as Raw
+import BDD.Raw
 
 import Control.Exception (finally)
 import Control.Monad (liftM)
@@ -12,21 +12,21 @@ import Test.QuickCheck (Property)
 -- creation and destruction.
 withBddMgr
   :: Int                  -- ^ the number of variables
-  -> (Raw.BddMgr -> IO a) -- ^ the action to run
+  -> (BddMgr -> IO a) -- ^ the action to run
   -> IO a
 withBddMgr numVars act = do
-    mgr <- Raw.bdd_mgr_create (fromIntegral numVars)
-    act mgr `finally` Raw.bdd_mgr_destroy mgr
+    mgr <- bdd_mgr_create (fromIntegral numVars)
+    act mgr `finally` bdd_mgr_destroy mgr
 
 -- | Converts the given IO Boolean action into a QuickCheck property.
 ioProperty :: IO Bool -> Property
 ioProperty test = monadicIO (run test >>= assert)
 
-bddProp :: (Raw.BddMgr -> BoolExpr -> IO Bool) -> AnnBoolExpr -> Property
+bddProp :: (BddMgr -> BoolExpr -> IO Bool) -> AnnBoolExpr -> Property
 bddProp prop (Ann (numVars, expr)) =
     ioProperty $ withBddMgr numVars $ flip prop expr
 
-bddProp' :: (Raw.BddMgr -> Int -> BoolExpr -> IO Bool) -> AnnBoolExpr -> Property
+bddProp' :: (BddMgr -> Int -> BoolExpr -> IO Bool) -> AnnBoolExpr -> Property
 bddProp' prop (Ann (numVars, expr)) =
     ioProperty $ withBddMgr numVars $ \mgr -> prop mgr numVars expr
 
@@ -50,15 +50,15 @@ prop_bddSymbolicEvalOk = bddProp' $ \mgr numVars expr -> do
 
 prop_initialNumNodes :: Int -> Property
 prop_initialNumNodes numVars = ioProperty $ withBddMgr numVars $ \mgr -> do
-    liftM (2 ==) (Raw.bdd_mgr_get_num_nodes mgr)
+    liftM (2 ==) (bdd_mgr_get_num_nodes mgr)
 
 prop_correctNumVars :: Int -> Property
 prop_correctNumVars numVars = ioProperty $ withBddMgr numVars $ \mgr -> do
-    liftM (fromIntegral numVars ==) (Raw.bdd_mgr_get_num_vars mgr)
+    liftM (fromIntegral numVars ==) (bdd_mgr_get_num_vars mgr)
 
 testIdempotent
-  :: (Raw.BddMgr -> Raw.Bdd -> Raw.Bdd -> IO Raw.Bdd)
-  -> Raw.BddMgr
+  :: (BddMgr -> Bdd -> Bdd -> IO Bdd)
+  -> BddMgr
   -> BoolExpr
   -> IO Bool
 testIdempotent op mgr expr = do
@@ -67,44 +67,44 @@ testIdempotent op mgr expr = do
     return (bdd == bdd')
 
 prop_andIdempotent :: AnnBoolExpr -> Property
-prop_andIdempotent = bddProp $ testIdempotent Raw.bdd_and
+prop_andIdempotent = bddProp $ testIdempotent bdd_and
 
 prop_orIdempotent :: AnnBoolExpr -> Property
-prop_orIdempotent = bddProp $ testIdempotent Raw.bdd_or
+prop_orIdempotent = bddProp $ testIdempotent bdd_or
 
 prop_selfImplies :: AnnBoolExpr -> Property
 prop_selfImplies = bddProp $ \mgr expr -> do
     bdd <- buildBdd mgr expr
-    bdd' <- Raw.bdd_implies mgr bdd bdd
-    return (bdd' == Raw.bdd_true)
+    bdd' <- bdd_implies mgr bdd bdd
+    return (bdd' == bdd_true)
 
 prop_selfEquiv :: AnnBoolExpr -> Property
 prop_selfEquiv = bddProp $ \mgr expr -> do
     bdd <- buildBdd mgr expr
-    bdd' <- Raw.bdd_equiv mgr bdd bdd
-    return (bdd' == Raw.bdd_true)
+    bdd' <- bdd_equiv mgr bdd bdd
+    return (bdd' == bdd_true)
 
 prop_selfXOr :: AnnBoolExpr -> Property
 prop_selfXOr = bddProp $ \mgr expr -> do
     bdd <- buildBdd mgr expr
-    bdd' <- Raw.bdd_xor mgr bdd bdd
-    return (bdd' == Raw.bdd_false)
+    bdd' <- bdd_xor mgr bdd bdd
+    return (bdd' == bdd_false)
 
 prop_not :: AnnBoolExpr -> Property
 prop_not = bddProp $ \mgr expr -> do
     bdd <- buildBdd mgr expr
-    bdd' <- Raw.bdd_not mgr bdd
+    bdd' <- bdd_not mgr bdd
     return (bdd /= bdd')
 
 prop_doubleNegation :: AnnBoolExpr -> Property
 prop_doubleNegation = bddProp $ \mgr expr -> do
     bdd <- buildBdd mgr expr
-    bdd' <- Raw.bdd_not mgr =<< Raw.bdd_not mgr bdd
+    bdd' <- bdd_not mgr =<< bdd_not mgr bdd
     return (bdd == bdd')
 
 prop_satCount :: AnnBoolExpr -> Property
 prop_satCount = bddProp' $ \mgr numVars expr -> do
     let numSols = length . filter (eval expr) . assignments $ numVars
     bdd <- buildBdd mgr expr
-    numSols' <- Raw.bdd_sat_count mgr bdd
+    numSols' <- bdd_sat_count mgr bdd
     return (fromIntegral numSols == numSols')
