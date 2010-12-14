e990de3662e21c319d2cfaa49bcbf81998f1783c
Bradford Larsen <brad.larsen@gmail.com>
2010-12-13 20:44:37 -0500

    More-or-less verbatim translation of N-queens from BuDDy.
    Much faster than my earlier encoding.

--- hs-bdd-test/TestNQueens.hs
+++ hs-bdd-test/TestNQueens.hs
@@ -5,17 +5,18 @@ import BDD.Raw
 import BDD.Util
 import NQueens
 
-import Control.Monad (liftM)
+import Control.Monad (liftM, forM_, when)
 import System.Environment (getProgName, getArgs)
 import System.Exit (exitFailure)
 import Text.Printf (hPrintf, printf)
 import System.IO (stderr)
+import Data.IORef (newIORef, readIORef, writeIORef)
 
 main :: IO ()
-main = getN >>= testQueensEquiv
+main = getParams >>= testQueensEquiv
 
-getN :: IO Int
-getN = do
+getParams :: IO Int
+getParams = do
     args <- getArgs
     case args of
         [n] -> case reads n of
@@ -28,13 +29,27 @@ usage = do
     hPrintf stderr "usage: %s N\n" =<< getProgName
     exitFailure
 
+-- testQueensEquiv :: Int -> IO ()
+-- testQueensEquiv n = do
+--     let (expr, _decoder) = queens n
+--     let nVars = numVars expr :: Int
+--     withBddMgr nVars $ \mgr -> do
+--         bdd <- buildBdd mgr expr
+--         numSols <- liftM realToFrac (bdd_sat_count mgr bdd) :: IO Double
+--         putStr . show =<< bdd_mgr_get_num_nodes mgr
+--         putStrLn " nodes in use"
+--         case numDistinctSolutions n of
+--             Just s | fromIntegral s /= numSols -> do
+--                 printf "FAILED: wrong number of solutions---expected %d, got %d\n"
+--                        s (round numSols :: Int)
+--                 exitFailure
+--             _ -> return ()
 
 testQueensEquiv :: Int -> IO ()
 testQueensEquiv n = do
-    let (expr, _decoder) = queens n
-    let nVars = numVars expr :: Int
+    let nVars = n * n
     withBddMgr nVars $ \mgr -> do
-        bdd <- buildBdd mgr expr
+        bdd <- buildQueens n mgr
         numSols <- liftM realToFrac (bdd_sat_count mgr bdd) :: IO Double
         putStr . show =<< bdd_mgr_get_num_nodes mgr
         putStrLn " nodes in use"
@@ -44,3 +59,72 @@ testQueensEquiv n = do
                        s (round numSols :: Int)
                 exitFailure
             _ -> return ()
+
+
+-- This is nearly a verbatim translation from the C++ N-queens
+-- encoding in the BuDDy 2.4 examples.  Way more efficient than my
+-- earlier encoding, for some reason.
+buildQueens :: Int -> BddMgr -> IO Bdd
+buildQueens nVars mgr = do
+    let n = fromIntegral nVars
+        loc i j = bdd_mgr_get_ith_var mgr (i * n + j)
+
+    queen <- newIORef bdd_true
+
+    -- place a queen in each row
+    forM_ [0..n-1] $ \i -> do
+        e <- newIORef bdd_false
+        forM_ [0..n-1] $ \j -> do
+            eVal <- readIORef e
+            writeIORef e =<< bdd_or mgr eVal =<< loc i j
+        queenVal <- readIORef queen
+        writeIORef queen =<< bdd_and mgr queenVal =<< readIORef e
+
+    -- build requirements for each variable
+    forM_ [0..n-1] $ \i -> do
+        forM_ [0..n-1] $ \j -> do
+            a <- newIORef bdd_true
+            b <- newIORef bdd_true
+            c <- newIORef bdd_true
+            d <- newIORef bdd_true
+
+            forM_ [0..n-1] $ \k -> do
+                when (k /= j) $ do
+                    aVal <- readIORef a
+                    xij <- loc i j
+                    nxik <- bdd_not mgr =<< loc i k
+                    writeIORef a =<< bdd_and mgr aVal =<< bdd_implies mgr xij nxik
+
+            forM_ [0..n-1] $ \k -> do
+                when (k /= i) $ do
+                    bVal <- readIORef b
+                    xij <- loc i j
+                    nxkj <- bdd_not mgr =<< loc k j
+                    writeIORef b =<< bdd_and mgr bVal =<< bdd_implies mgr xij nxkj
+
+            forM_ [0..n-1] $ \k -> do
+                let ll = k - i + j;
+                when (ll >= 0 && ll < n && k /= i) $ do
+                    cVal <- readIORef c
+                    xij <- loc i j
+                    nxkll <- bdd_not mgr =<< loc k ll
+                    writeIORef c =<< bdd_and mgr cVal =<< bdd_implies mgr xij nxkll
+
+            forM_ [0..n-1] $ \k -> do
+                let ll = i + j - k
+                when (ll >= 0 && ll < n && k /= i) $ do
+                    dVal <- readIORef d
+                    xij <- loc i j
+                    nxkll <- bdd_not mgr =<< loc k ll
+                    writeIORef d =<< bdd_and mgr dVal =<< bdd_implies mgr xij nxkll
+
+            queenVal <- readIORef queen
+            aVal <- readIORef a
+            bVal <- readIORef b
+            cVal <- readIORef c
+            dVal <- readIORef d
+            writeIORef queen =<< bdd_and mgr queenVal
+                             =<< bdd_and mgr aVal
+                             =<< bdd_and mgr bVal
+                             =<< bdd_and mgr cVal dVal
+    readIORef queen
