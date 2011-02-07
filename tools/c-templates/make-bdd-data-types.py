94fccd782cb010356234483cc906cd8fa5ba708c
Bradford Larsen <brad.larsen@gmail.com>
2011-02-06 19:52:34 -0500

    Generate just a hash table for nodes.

--- tools/c-templates/make-bdd-data-types.py
+++ tools/c-templates/make-bdd-data-types.py
@@ -1,61 +1,16 @@
 from instantiate import make_hash_table
 
 make_hash_table(
-    include_guard_name='BDD_RTU_INCLUDED',
-    local_includes='#include "bddlib.h"\n#include "raw_bdd.h"\n#include "memory.h"',
-    prefix='bdd_rtu_ht',
-    hash_table_type='bdd_rtu_ht_t',
-    key_type='raw_bdd_t',
-    key_equal_func='raw_bdd_equal',
-    key_hash_func='raw_bdd_hash',
-    value_type='bdd_t *',
-    header_name='bdd_rtu_ht.h',
-    code_name='bdd_rtu_ht.c',
-    malloc_func='checked_malloc',
-    free_func='checked_free',
-    )
-
-make_hash_table(
-    include_guard_name='BDD_HT_INCLUDED',
-    local_includes='#include "bddlib.h"\n#include "raw_bdd.h"\n#include "memory.h"',
-    prefix='bdd_ht',
-    hash_table_type='bdd_ht_t',
-    key_type='raw_bdd_t',
-    key_equal_func='raw_bdd_equal',
-    key_hash_func='raw_bdd_hash',
-    value_type='raw_bdd_t',
-    header_name='bdd_ht.h',
-    code_name='bdd_ht.c',
-    malloc_func='checked_malloc',
-    free_func='checked_free',
-    )
-
-make_hash_table(
-    include_guard_name='BDD_DOUBLE_HT_INCLUDED',
-    local_includes='#include "bddlib.h"\n#include "raw_bdd.h"\n#include "memory.h"',
-    prefix='bdd_double_ht',
-    hash_table_type='bdd_double_ht_t',
-    key_type='raw_bdd_t',
-    key_equal_func='raw_bdd_equal',
-    key_hash_func='raw_bdd_hash',
-    value_type='double',
-    header_name='bdd_double_ht.h',
-    code_name='bdd_double_ht.c',
-    malloc_func='checked_malloc',
-    free_func='checked_free',
-    )
-
-make_hash_table(
-    include_guard_name='USR_BDD_HT_INCLUDED',
-    local_includes='#include "bddlib.h"\n#include "memory.h"\n#include "bdd.h"\n#include "usr_bdd_entry.h"',
-    prefix='usr_bdd_ht',
-    hash_table_type='usr_bdd_ht_t',
-    key_type='bdd_t *',
-    key_equal_func='bdd_ptr_equal',
-    key_hash_func='bdd_ptr_hash',
-    value_type='usr_bdd_entry_t',
-    header_name='usr_bdd_ht.h',
-    code_name='usr_bdd_ht.c',
+    include_guard_name='NODE_HT_INCLUDED',
+    local_includes='#include "bdd_mgr.h"\n#include "memory.h"\n',
+    prefix='node_ht',
+    hash_table_type='node_ht_t',
+    key_type='node_t',
+    key_equal_func='node_equal',
+    key_hash_func='node_hash',
+    value_type='unsigned',
+    header_name='node_ht.h',
+    code_name='node_ht.c',
     malloc_func='checked_malloc',
     free_func='checked_free',
     )
